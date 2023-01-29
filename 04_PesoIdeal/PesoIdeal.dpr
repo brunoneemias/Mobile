a2336c21c818311cdd134f1be21efd285b59987a
Bruno Neemias <89363512+brunoneemias@users.noreply.github.com>
2023-01-29 00:34:49 -0300

    Add files via upload

---
+++ 04_PesoIdeal/PesoIdeal.dpr
@@ -0,0 +1,14 @@
+program PesoIdeal;
+
+uses
+  System.StartUpCopy,
+  FMX.Forms,
+  Unit1 in '..\Calculadora\Unit1.pas' {Form1};
+
+{$R *.res}
+
+begin
+  Application.Initialize;
+  Application.CreateForm(TForm1, Form1);
+  Application.Run;
+end.
---
+++ 04_PesoIdeal/Unit1.pas
@@ -0,0 +1,26 @@
+unit Unit1;
+
+interface
+
+uses
+  System.SysUtils, System.Types, System.UITypes, System.Classes, System.Variants,
+  FMX.Types, FMX.Controls, FMX.Forms, FMX.Graphics, FMX.Dialogs, FMX.StdCtrls,
+  FMX.Controls.Presentation, FMX.Edit;
+
+type
+  TForm1 = class(TForm)
+  private
+    { Private declarations }
+  public
+    { Public declarations }
+  end;
+
+var
+  Form1: TForm1;
+  soma, sub, divi, multi, n1, n2: double;
+
+implementation
+
+{$R *.fmx}
+
+end.
